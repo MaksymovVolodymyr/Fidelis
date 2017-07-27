b579c54674caa847d2cd637a7c03b1a16bd71a08
Vladimir Maksimov <maksimov_vd@mail.ua>
2017-07-27 17:22:29 +0300

    serv

--- js/script.js
+++ js/script.js
@@ -23,7 +23,6 @@
         work = true;
     };
     document.addEventListener("DOMNodeInserted", showSlider);
-    var slNumb = 3;
 
     function showSlider() {
         if( slider === null || work === true) {
@@ -31,16 +30,17 @@
         };
         work = true;
         slider = document.querySelector('.top_slider_wrapper');
-        var slids = document.querySelectorAll('.sub_articl');
-        var slides = [];
-        for(var i = 0; i < 3; i++) {
-            slides[i] = slids[i].cloneNode(true);
-            slider.appendChild(slides[i]);
+        var container = document.querySelector('.slider');
+        var stWidth = container.offsetWidth;
+        var slides = slider.querySelectorAll('.sub_articl');
+        for(var i = 0; i < slides.length; i++) {
+            slides[i].style.width = stWidth +'px';
             if (i > 0) {
                 slides[i].style.marginLeft = slides[i].offsetWidth + 'px';
             }
         };
         var counter = 0;
+        var slNumb = slides.length;
         var roundsBox = document.querySelector('.circle_wrapper');
         var n = 0;
         for (var i = 0; i < slides.length; i++ ) {
@@ -113,6 +113,114 @@
 
         roundsBox.addEventListener('click', showSlide);
         setInterval( demo, 6000);
+
+    };
+})();
+(function(){
+    var slider = document.querySelector('.about_slider_wrapper');
+    var work = false;
+    if (slider !== null) {
+        if (work === true) return;
+        showSlider();
+        work = true;
+    };
+    document.addEventListener("DOMNodeInserted", showSlider);
+    var slNumb = 3;
+
+    function showSlider() {
+        if( slider === null || work === true) {
+            return;
+        };
+        work = true;
+        slider = document.querySelector('.about_slider_wrapper');
+//        var slids = document.querySelectorAll('.sub_articl');
+//        var slides = [];
+//        for(var i = 0; i < 3; i++) {
+//            slides[i] = slids[i].cloneNode(true);
+//            slider.appendChild(slides[i]);
+//            if (i > 0) {
+//                slides[i].style.marginLeft = slides[i].offsetWidth + 'px';
+//            }
+//        };
+        var slides = document.querySelectorAll('.about_slider_wrapper img');
+                for(var i = 0; i < slides.length; i++) {
+            if (i > 0) {
+                slides[i].style.marginLeft = slides[i].offsetWidth + 'px';
+            }
+        };
+        var counter = 0;
+        var roundsBox = document.querySelector('.disk_wrapper');
+        var n = 0;
+        for (var i = 0; i < slides.length; i++ ) {
+            var round = document.createElement('div');
+            round.value = i;
+            roundsBox.appendChild(round);
+        };
+        var status = true;
+        var rounds = document.querySelectorAll('.disk_wrapper div');
+        rounds[0].style.backgroundColor = '#00293f';
+        function showSlide(ev) {
+            if (status === false) return;
+            status = false;
+            for (var i = 0; i < rounds.length; i++) {
+                rounds[i].style.backgroundColor = 'transparent';
+            };
+            var goal;
+            if (ev === undefined && counter < rounds.length) {
+                goal = rounds[counter + 1];
+            }
+            if (ev === undefined && counter === rounds.length - 1) {
+                goal = rounds[0];
+            }
+            if (ev) {
+                goal = ev.target;
+                if (ev.target === roundsBox) {
+                    goal = rounds[n];
+                };
+            }
+            n = goal.value;
+            goal.style.backgroundColor = "#00293f";
+            if ( counter === slNumb - 1) {
+                if (n > 0) {
+                    var beforeEl = slides[counter].offsetWidth + "px";
+                    slides[n].style.marginLeft = - slides[n].offsetWidth + "px";
+                    Velocity( slides[n], { "margin-left" : 0 }, 1000);
+                    Velocity( slides[counter], {"margin-left" : beforeEl }, 1000);
+                } else {
+                    var beforeEl = - slides[counter].offsetWidth + "px";
+                    slides[0].style.marginLeft = slides[0].offsetWidth + "px";
+                    Velocity( slides[0], { "margin-left" : 0 }, 1000);
+                    Velocity( slides[counter], {"margin-left" : beforeEl }, 1000);
+                    n = 0;
+                }
+            }
+            if(n < counter && counter !== slNumb - 1) {
+                var beforeEl = slides[counter].offsetWidth + "px";
+                slides[n].style.marginLeft = - slides[n].offsetWidth + "px";
+                Velocity( slides[n], { "margin-left" : 0 }, 1000);
+                Velocity( slides[counter], {"margin-left" : beforeEl }, 1000);
+            };
+            if (n > counter && counter !== slNumb - 1){
+                var beforeEl = - slides[counter].offsetWidth + "px";
+                slides[n].style.marginLeft = slides[n].offsetWidth + "px";
+                Velocity( slides[n], { "margin-left" : 0 }, 1000);
+                Velocity( slides[counter], {"margin-left" : beforeEl }, 1000);
+            };
+            counter = n;
+            function changeStatus() {
+                status = true;
+            };
+            setTimeout( changeStatus , 1050);
+
+        };
+        function demo() {
+            if(status === true) {
+                showSlide();
+            }
+        };
+
+        roundsBox.addEventListener('click', showSlide);
+        setInterval( demo, 5000);
 
     };
 })();
@@ -238,9 +346,72 @@
         }
     }
     gamb.addEventListener('click', show);
-
 })();
-
-  document.getElementById('lang').onclick = function() {
-      document.getElementById('lang').style.height = '40px';
+(function(){
+    var slider = document.querySelector('.brand');
+    var work = false;
+    if (slider !== null) {
+        if (work === true) return;
+        showSlider();
+        work = true;
+    };
+    document.addEventListener("DOMNodeInserted", showSlider);
+    var slNumb = 3;
+    var la = document.querySelector('.brand_la');
+    var ra = document.querySelector('.brand_ra');
+    var counter;
+    var correction;
+    var viewer;
+    var slidesLength;
+    function showSlider() {
+        if( slider === null || work === true) {
+            return;
+        };
+        work = true;
+        var newSlide;
+        viewer = document.querySelector('.brand_viewer');
+        var slides = viewer.querySelectorAll('a');
+        slidesLength = slides.length;
+        counter = slides.length;
+                var firstSlide = slides[0];
+        for(var i = 0; i < slidesLength; i++) {
+            newSlide = slides[i].cloneNode(true);
+            viewer.appendChild(newSlide);
+        }
+        for(var i = 0; i < slidesLength; i++) {
+            newSlide = slides[i].cloneNode(true);
+            viewer.insertBefore(newSlide, firstSlide);
+        }
+        correction = slides[0].offsetWidth;
+        viewer.style.marginLeft = -correction*counter + 'px';
+    };
+    function moveLeft(ev) {
+        counter--; 
+        console.log(counter);
+        var position = -correction*counter + 'px';
+         Velocity( viewer, { "marginLeft" : position }, 600);
+        if(counter === 0) {
+            setTimeout( function(){
+                console.log('start');
+            counter = slidesLength;
+            viewer.style.marginLeft = -correction*counter + 'px';
+            }, 600);
+            
+        }
     }
+    function moveRight(ev) {
+        counter++; 
+        var position = -correction*counter + 'px';
+         Velocity( viewer, { "marginLeft" : position }, 600);
+            if(counter === 2*slidesLength) {
+            setTimeout( function(){
+            console.log('start2');
+            counter = slidesLength;
+            viewer.style.marginLeft = -correction*counter + 'px';
+            }, 600);
+            
+        }
+    }
+    la.addEventListener('click', moveLeft);
+    ra.addEventListener('click', moveRight);
+})();
